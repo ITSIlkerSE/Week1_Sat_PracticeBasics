f2a1a3320fd5837224b86930d12b46a59499ef1a
itsilkerse <itsilkerse@gmail.com>
2022-09-03 17:26:25 +0200

    Repeating fizzbuzz

--- src/main/java/PracticeBasics.java
+++ src/main/java/PracticeBasics.java
@@ -1,2 +1,26 @@
 public class PracticeBasics {
+    public static void main(String[] args) {
+
+        System.out.println(fizzBuzz(15));
+
+    }
+
+    public static String fizzBuzz (int number){
+
+        boolean isDividableBy3And5 = number % 3 == 0 && number % 5 == 0;
+        boolean isDividableBy3 = number % 3 == 0;
+        boolean isDividableBy5 = number % 5 == 0;
+
+        if(isDividableBy3And5){
+            return "fizzbuzz";
+        } else if (isDividableBy5) {
+            return "buzz";
+        } else if (isDividableBy3) {
+            return "fizz";
+        }
+
+
+        return String.valueOf(number);
+    }
+
 }
--- src/test/java/PracticeBasicsTest.java
+++ src/test/java/PracticeBasicsTest.java
@@ -1,10 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PracticeBasicsTest {
 
     @Test
+    void testFor15()
 
+    {
 
+        //Given
+        int number = 15;
+        //When
+        String actual =  PracticeBasics.fizzBuzz(number);
+        //Then
+        assertEquals("fizzbuzz", actual);
 
-
+    }
 }
